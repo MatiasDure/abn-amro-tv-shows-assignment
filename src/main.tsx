7f898bfbe94efd41952f762ae3a1d3b0472575cc
Matias Dure <matidure1997@gmail.com>
2026-02-24 18:27:23 +0100

    Added basename to browser router

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./styles/global.scss"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/abn-amro-tv-shows-assignment">
       <App />
     </BrowserRouter>
   </StrictMode>,
